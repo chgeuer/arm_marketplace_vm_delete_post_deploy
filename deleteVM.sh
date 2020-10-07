f3b2e37b79bdfaa460131a06c203a9e76c86c6aa
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2020-10-07 17:00:44 +0200

    .

--- deleteVM.sh
+++ deleteVM.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Greeting was \"${GREETINGS}\" -- ${AZ_SCRIPTS_OUTPUT_PATH}"
-
 curl \
    --request GET \
    --location \
@@ -17,6 +15,9 @@ disk_output="$(  az disk              delete --yes --no-wait --ids "${DUMMY_DISK
 nic_output="$(   az network nic       delete       --no-wait --ids "${DUMMY_NIC}"   )"
 pip_output="$(   az network public-ip delete                 --ids "${DUMMY_IP}"    )"
 
+#
+# These delete operations return an empty response on success
+#
 if [[ ${vm_output} == "" ]];   then vm_output="\"Success\""; fi;
 if [[ ${disk_output} == "" ]]; then disk_output="\"Success\""; fi;
 if [[ ${nic_output} == "" ]];  then nic_output="\"Success\""; fi;
@@ -34,12 +35,5 @@ output="$( echo "{}" | \
     jq --arg x "${pip_output}"   '.output.pip=($x | fromjson)'  | \
     jq --arg x "${ip}"           '.ip=$x'                         )"
 
-AZ_SCRIPTS_OUTPUT_PATH="scriptoutputs.json"
-
-
 # https://docs.microsoft.com/en-us/azure/azure-resource-manager/templates/deployment-script-template?tabs=CLI#work-with-outputs-from-cli-script
 echo "${output}" > "${AZ_SCRIPTS_OUTPUT_PATH}"
-
-echo "XXX ${output}"
-echo "AZ_SCRIPTS_OUTPUT_PATH content: "
-cat "${AZ_SCRIPTS_OUTPUT_PATH}"
