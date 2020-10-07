4411cc20c361b2643943a45dac5b954cebd70a8e
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2020-10-07 13:24:08 +0200

    .

--- deleteVM.sh
+++ deleteVM.sh
@@ -2,6 +2,8 @@
 
 echo "Greeting was \"${GREETINGS}\""
 
+echo "AZ_SCRIPTS_OUTPUT_PATH = \"${AZ_SCRIPTS_OUTPUT_PATH}\"
+
 echo "Downloading jq"
 curl \
    --request GET \
@@ -10,23 +12,34 @@ curl \
    --url "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64" \
    --output ./jq && chmod +x ./jq
 
+alias jq="./jq"
+
 echo "Logging in with managed identity"
 echo "$( az login --identity )"
 
 echo "###################"
 echo "VM to delete is \"${DUMMY_VM_ID}\""
-echo "$( az vm delete --verbose --yes --ids "${DUMMY_VM_ID}" )"
+vm_output="$( az vm delete ----yes --ids "${DUMMY_VM_ID}" )"
 
-echo "###################"
 echo "Managed disk to delete is \"${DUMMY_DISK}\""
-echo "$( az disk delete --verbose --yes --no-wait --ids "${DUMMY_DISK}" )"
+disk_output="$( az disk delete --yes --no-wait --ids "${DUMMY_DISK}" )"
 
-echo "###################"
 echo "NIC to delete is \"${DUMMY_NIC}\""
-echo "$( az network nic delete --verbose --no-wait --ids "${DUMMY_NIC}" )"
+nic_output="$( az network nic delete --no-wait --ids "${DUMMY_NIC}" )"
 
-echo "###################"
 echo "Public IP to delete is \"${DUMMY_IP}\""
-echo "$( az network public-ip delete --verbose --ids "${DUMMY_IP}" )"
+pip_output="$( az network public-ip delete --ids "${DUMMY_IP}" )"
+
+# https://docs.microsoft.com/en-us/azure/azure-resource-manager/templates/deployment-script-template?tabs=CLI#work-with-outputs-from-cli-script
+
+output="$( echo "{}" | \
+    jq ".output.vm_output=\"${vm_output}\"" |  \
+    jq ".output.disk_output=\"${disk_output}\"" |  \
+    jq ".output.nic_output=\"${nic_output}\"" |  \
+    jq ".output.pip_output=\"${pip_output}\"" \
+    )"
+
+echo "${output}" > "${AZ_SCRIPTS_OUTPUT_PATH}"
+echo "AZ_SCRIPTS_OUTPUT_PATH content: $(cat "${AZ_SCRIPTS_OUTPUT_PATH}" )"
 
 echo "DONE... Good bye"
