f3c9e25237ee71f9aae9e317f67c22f6f86828ae
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2020-10-07 11:44:20 +0200

    .

--- deleteVM.sh
+++ deleteVM.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-ls -als
+echo "Downloading jq"
+curl \
+   --request GET \
+   --location \
+   --silent \
+   --url "https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64" \
+   --output ./jq && chmod +x ./jq
+
+echo "Logging in with managed identity"
+az login --identity
+
+echo "Greeting was \"${GREETINGS}\""
+
+echo "VM to delete is \"${DUMMY_VM_ID}\""
