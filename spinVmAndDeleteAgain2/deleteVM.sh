961dd37b49df074f0e4ded5b24899fc706d0c172
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2020-10-09 09:43:59 +0200

    .

--- spinVmAndDeleteAgain2/deleteVM.sh
+++ spinVmAndDeleteAgain2/deleteVM.sh
@@ -13,7 +13,6 @@ login_output="$( az login --identity )"
 vm_output="$(    az vm                delete --yes           --ids "${DUMMY_VM_ID}" )"
 disk_output="$(  az disk              delete --yes --no-wait --ids "${DUMMY_DISK}"  )"
 nic_output="$(   az network nic       delete       --no-wait --ids "${DUMMY_NIC}"   )"
-pip_output="$(   az network public-ip delete                 --ids "${DUMMY_IP}"    )"
 
 #
 # These delete operations return an empty response on success
@@ -21,7 +20,6 @@ pip_output="$(   az network public-ip delete                 --ids "${DUMMY_IP}"
 if [[ ${vm_output} == "" ]];   then vm_output="{}"; fi;
 if [[ ${disk_output} == "" ]]; then disk_output="{}"; fi;
 if [[ ${nic_output} == "" ]];  then nic_output="{}"; fi;
-if [[ ${pip_output} == "" ]];  then pip_output="{}"; fi;
 
 # Hit some external service
 ip="$( curl --silent --url "https://postman-echo.com/ip" | jq -r ".ip" )"
@@ -32,7 +30,6 @@ output="$( echo "{}" | \
     jq --arg x "${vm_output}"    '.tasks.vm=($x | fromjson)'    | \
     jq --arg x "${disk_output}"  '.tasks.disk=($x | fromjson)'  | \
     jq --arg x "${nic_output}"   '.tasks.nic=($x | fromjson)'   | \
-    jq --arg x "${pip_output}"   '.tasks.pip=($x | fromjson)'   | \
     jq --arg x "${ip}"           '.ip=$x'                       )"
 
 # https://docs.microsoft.com/en-us/azure/azure-resource-manager/templates/deployment-script-template?tabs=CLI#work-with-outputs-from-cli-script
