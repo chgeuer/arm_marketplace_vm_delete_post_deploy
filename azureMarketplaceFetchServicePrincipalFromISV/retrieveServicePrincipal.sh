483e907da0b1c9a1d5a93b3dc0d0ef1cd1175fb0
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2020-10-09 15:34:10 +0200

    .

--- azureMarketplaceFetchServicePrincipalFromISV/retrieveServicePrincipal.sh
+++ azureMarketplaceFetchServicePrincipalFromISV/retrieveServicePrincipal.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+## Unnecessary. `jq` is already on the box
 # curl \
 #    --request GET \
 #    --location \
@@ -25,13 +26,20 @@ servicePrincipalDetails="$( \
 	--data "${azure_env}" \
     )"
 
+
+# TODO MUST MATCH REAL API, we're getting garbage out of postman here
+RESPONSE_PATH_CLIENT_ID=".headers.host"
+RESPONSE_PATH_CLIENT_SECRET=".data.deployment.properties.templateHash"
+
+client_id="$( echo "${servicePrincipalDetails}" | jq -r "${RESPONSE_PATH_CLIENT_ID}" )"
+client_secret="$( echo "${servicePrincipalDetails}" | jq -r "${RESPONSE_PATH_CLIENT_SECRET}" )"
+
 output="$( \
   echo "{}" \
     | jq --arg x "${azure_env}"               '.environment=($x | fromjson)' \
     | jq --arg x "${servicePrincipalDetails}" '.servicePrincipalDetails=($x | fromjson)' \
+    | jq --arg x "${client_id}"               '.servicePrincipal.client_id=($x | fromjson)' \
+    | jq --arg x "${client_secret}"           '.servicePrincipal.client_secret=($x | fromjson)' \
 	)"
-
-# echo "${servicePrincipalDetails}" | jq "."
-# echo "${output}" | jq "."
 
 echo "${output}" > "${AZ_SCRIPTS_OUTPUT_PATH}"
